1eb8d83df1c849451353a61ae485c64b3e66a3d2
Fernando Sandoval <sandovalf193@gmail.com>
2020-07-24 19:17:34 -0600

    loading fixed

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from 'wouter';
 
 import UseContacts from 'hooks/useContacts';
